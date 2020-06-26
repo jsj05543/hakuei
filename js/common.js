f1a7d50a26a69236b379125a52031576cb39a218
jsj05543 <bai212ying@163.com>
2020-06-27 03:35:42 +0900

    更新 6/27

---
+++ js/common.js
@@ -0,0 +1,15 @@
+$(function () {
+  $(function () {
+    // Shift-JISに変更する場合
+    $.ajaxSetup({
+      beforeSend: function (xhr) {
+        xhr.overrideMimeType("text/html;charset=Shift_JIS");
+      }
+    });
+  });
+  $("#header").load("sub/header.html");
+  $("#sidebarMenu").load("sub/menu.html");
+  $('<input type="checkbox" id="openSidebarMenu" class="openSidebarMenu">').insertAfter('#header');
+  $('<label for="openSidebarMenu" class="sidebarIconToggle"><div class="spinner diagonal part-1"></div><div class="spinner horizontal"></div><div class="spinner diagonal part-2"></div></label>').insertBefore('#sidebarMenu');
+  
+});
