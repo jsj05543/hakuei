0f36a6a277bebaa0e58c56d8bd0e4f4d6b54a306
jsj05543 <bai212ying@163.com>
2020-06-30 00:56:05 +0900

    更新

--- js/common.js
+++ js/common.js
@@ -12,4 +12,27 @@ $(function () {
   $('<input type="checkbox" id="openSidebarMenu" class="openSidebarMenu">').insertAfter('#header');
   $('<label for="openSidebarMenu" class="sidebarIconToggle"><div class="spinner diagonal part-1"></div><div class="spinner horizontal"></div><div class="spinner diagonal part-2"></div></label>').insertBefore('#sidebarMenu');
   // $("#main").load("brgd0020.html");
+  $( 'input[name="exampleRadios"]:radio' ).change( function() {
+    var radioval = $(this).val();
+    if(radioval == "1"){
+        var color = "rgb(81, 73, 192)";
+      $('#color1').css("background-color", color);
+      $('#header').css("background-color",color);
+        
+
+        console.log("1");
+    }
+    if(radioval == "2"){
+        var color = "rgb(192, 73, 73)";
+      $('#color1').css("background-color", color);
+      $('#header').css("background-color",color);
+    }
+    if(radioval == "3"){
+        var color = "rgb(101, 192, 73)";
+      $('#color1').css("background-color", color);
+      $('#header').css("background-color",color);
+        
+        console.log("3");
+    }
+});
 });
